10f05075df112d2463db92c8fb9591afc60a4ecc
Alan Chen <alanc1995@gmail.com>
2017-10-01 00:45:31 -0400

    Just react

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Boilerplate
+React and ES6
+
+## Run
+- npm install --save-dev
+- npm run webpack
+- npm run start
---
+++ static/js/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+
+class Hello extends React.Component {
+	render() {
+    	return <div>
+        <h3>Hello World</h3>
+    		<h3>Distance: {_.get(this.props, 'sensors.distance')}</h3>
+    		<h3>Temperature: {_.get(this.props, 'sensors.temperature')}</h3>
+    	</div>
+  	}
+}
+
+ReactDOM.render(<Hello sensors = {window.props}/>, document.getElementById('root'));
