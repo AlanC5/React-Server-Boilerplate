35298b50153f4bedd5ae7d61fc923148e88cf344
Alan Chen <alanc1995@gmail.com>
2019-03-31 00:10:17 -0400

    Clean up

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder = "static/dist", template_folder = "static")
+app = Flask(__name__, static_folder = "static", template_folder = "static/dist")
 app.config['SECRET_KEY'] = 'secret!'
 
 @app.route('/')
@@ -13,4 +13,4 @@ def index():
     return render_template('index.html', sensors = sensors)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8090, debug=True)
